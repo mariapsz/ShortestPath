f2acfd6d1c1a49dce4d84fe2708b4fcb202ba415
mariapsz <mariapszonka999@gmail.com>
2019-05-07 23:27:48 +0200

    Unresolved problem with making matrix symetrical

--- src/components/map.js
+++ src/components/map.js
@@ -18,15 +18,28 @@ class Place {
 }
 
 const places = [
-    new Place('Warszawa', L.latLng(52.227932, 21.012843), ['Łódź', 'Białystok']),
-    new Place('Łódź', L.latLng(51.755869, 19.435534), ['Warszawa', 'Białystok']),
-    new Place('Białystok', L.latLng(53.133148, 23.167585), ['Łódź', 'Warszawa']),
+    new Place('Szczecin', L.latLng(53.444738, 14.524450), ['Gorzów Wielkopolski', 'Gdańsk', 'Bydgoszcz', 'Poznań']),
+    new Place('Poznań', L.latLng(52.410637, 16.918623), [ 'Szczecin', 'Gorzów Wielkopolski', 'Wrocław', 'Bydgoszcz','Gdańsk' ,'Łódź', 'Opole']),
+    new Place('Olsztyn', L.latLng(53.773239, 20.476377), ['Gdańsk', 'Białystok', 'Warszawa', 'Bydgoszcz']),
+    new Place('Kielce', L.latLng(50.864746, 20.628129), ['Warszawa', 'Lublin', 'Kraków', 'Łódź', 'Rzeszów', 'Katowice']),
+    new Place('Katowice', L.latLng(50.254167, 19.020610), ['Kraków', 'Opole', 'Kielce', 'Łódź']),
+    new Place('Gdańsk', L.latLng(54.379539, 18.622404), ['Szczecin', 'Poznań', 'Bydgoszcz', 'Olsztyn']),
+    new Place('Białystok', L.latLng(53.133148, 23.167585), ['Lublin', 'Warszawa', 'Bydgoszcz', 'Olsztyn']),
+    new Place('Rzeszów', L.latLng(50.031183, 22.003103), ['Kraków', 'Lublin', 'Kielce']),
+    new Place('Opole', L.latLng(50.670958, 17.925569), ['Wrocław', 'Poznań', 'Kraków', 'Łódź']),
+    new Place('Warszawa', L.latLng(52.227932, 21.012843), ['Łódź', 'Białystok', 'Kielce', 'Bydgoszcz', 'Lublin', 'Olsztyn']),
+    new Place('Kraków', L.latLng(50.062435, 19.959979), ['Katowice', 'Rzeszów', 'Kielce']),
+    new Place('Łódź', L.latLng(51.763257, 19.507721), ['Warszawa', 'Kielce', 'Opole', 'Katowice', 'Wrocław', 'Poznań', 'Bydgoszcz']),
+    new Place('Gorzów Wielkopolski', L.latLng(52.733179, 15.241680), ['Szczecin', 'Poznań', 'Wrocław']),
+    new Place('Lublin', L.latLng(51.234893, 22.570755), ['Łódź', 'Warszawa', 'Białystok' ,'Rzeszów', 'Kielce']),
+    new Place('Bydgoszcz', L.latLng(53.121995, 18.018519), ['Warszawa', 'Białystok', 'Olsztyn', 'Gdańsk', 'Szczecin', 'Poznań', 'Łódź']),
+    new Place('Wrocław', L.latLng(51.107865, 17.029611), ['Gorzów Wielkopolski', 'Poznań', 'Łódź', 'Opole']),
 ];
 
 
 class Map extends React.Component {
 
-    createAdjacencyMatrix() {
+    createAdjacencyMatrix(callback) {
         let matrix = [];
         let rows = places.length;
         let columns = places.length;
@@ -45,37 +58,50 @@ class Map extends React.Component {
         for (let j = 0; j < places.length; j++) {
             console.log('start: ' + j);
             for (let k = 0; k < places[j].connections.length; k++) {
+                //console.log('k: ' + k);
                 let connPlaceIdx = Map.getPlaceIndex(places[j].connections[k], places);
-                let waypoints = [
-                    places[j].latLng,
-                    places[connPlaceIdx].latLng,
-                ];
+                console.log('connPlaceIdx: ' + connPlaceIdx);
+                if (matrix[connPlaceIdx][j] !== 0) {
+                    console.log('czemu tu nigdy nie wchodzi? ;/');
+                    matrix[j][connPlaceIdx] = matrix[connPlaceIdx][j];
+                } else {
+                  //  console.log(matrix);
+                 //   console.log('connPlaceIdx: ' + connPlaceIdx + ' j: ' + j);
+                  //  console.log(matrix[connPlaceIdx][j]);
+                    let waypoints = [
+                        places[j].latLng,
+                        places[connPlaceIdx].latLng,
+                    ];
+                    callback();
+                    let routeControl = L.Routing.control({
+                        plan: L.Routing.plan(waypoints, {
+                            createMarker: function (i, wp) {
+                                return L.marker(wp.latLng, {
+                                    draggable: true,
+                                    icon: L.icon({
+                                        iconUrl: 'dot.png',
+                                        shadowUrl: 'dot.png',
+                                    }),
+                                });
+                            },
+                        }),
+                    }).addTo(this.map);
+                    callback();
 
-                let routeControl = L.Routing.control({
-                    plan: L.Routing.plan(waypoints, {
-                        createMarker: function (i, wp) {
-                            return L.marker(wp.latLng, {
-                                draggable: true,
-                                icon: L.icon({
-                                    iconUrl: 'dot.png',
-                                    shadowUrl: 'dot.png',
-                                }),
-                            });
-                        },
-                    }),
-                }).addTo(this.map);
-
-                routeControl.on('routesfound', function (e) {
-                    let routes = e.routes;
-                    let summary = routes[0].summary;
-                    console.log(summary.totalDistance / 1000);
-                    console.log('middle: ' + j);
-                    matrix[j][connPlaceIdx] = summary.totalDistance / 1000;
-                    console.log(matrix);
-                    //total time is ' + Math.round(summary.totalTime % 3600 / 60) + ' minutes');
-                });
+                    routeControl.on('routesfound', function (e) {
+                        let routes = e.routes;
+                        let summary = routes[0].summary;
+                        //console.log(summary.totalDistance / 1000);
+                        //console.log('middle: ' + j);
+                        matrix[j][connPlaceIdx] = summary.totalDistance / 1000;
+                        //console.log(matrix);
+                        //total time is ' + Math.round(summary.totalTime % 3600 / 60) + ' minutes');
+                    });
+                    callback();
+                }
             }
         }
+        return matrix;
     }
 
     static getPlaceIndex(name, array) {
@@ -101,7 +127,9 @@ class Map extends React.Component {
         // add layer
         this.layer = L.layerGroup().addTo(this.map);
 
-        this.createAdjacencyMatrix();
+        console.log(this.createAdjacencyMatrix(function () {
+            console.log('funkcja callback');
+        }));
     }
 
 
