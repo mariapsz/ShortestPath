76c2630210693db5fe0a8db9fd02a01c5aebb816
mariapsz <mariapszonka999@gmail.com>
2019-05-10 18:09:22 +0200

    sdfsd

--- src/components/map.js
+++ src/components/map.js
@@ -1,9 +1,10 @@
 import React from "react";
 import L from "leaflet";
-import "leaflet-routing-machine"
-import "leaflet.icon.glyph"
-import "./map.css"
+import "leaflet-routing-machine";
+import "leaflet.icon.glyph";
+import "./map.css";
 
+const fs = require('fs');
 
 class Place {
     constructor(name, latLng, connections) {
@@ -38,61 +39,127 @@ class Map extends React.Component {
     map;
     tempMap;
 
-    LatLangToArray(latLang) {
+    static GetPlaceIdx(placeName, array) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].name === placeName)
+                return i;
+        }
+        return -1;
+    }
+
+    RouterControlRoadsLinesToJSON = (JSONPath, places, map) => {
+        const roads = [];
+        places.forEach(function (place) {
+            place.connections.forEach(function (connection) {
+                let connectionIdx = Map.GetPlaceIdx(connection, places);
+                let waypoints = [place.latLng, places[connectionIdx].latLng];
+                let control = L.routing.control({
+                    waypoints,
+                }).addTo(map);
+                control.on('routeselected', (e) => {
+                    let road = {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "LineString",
+                            "coordinates": Map.ArrayOfLatLAngToArrayOfNumbers(e.route.coordinates)
+                        },
+                        "properties": {
+                            "color": "red"
+                        }
+                    };
+                    roads.push(road);
+                });
+
+            });
+        });
+        let data = {
+            "type": "FeatureCollection",
+            "features": roads,
+        };
+
+
+        setTimeout(() => {
+                function download(content, fileName, contentType) {
+                    let a = document.createElement("a");
+                    let file = new Blob([content], {type: contentType});
+                    a.href = URL.createObjectURL(file);
+                    a.download = fileName;
+                    a.click();
+                }
+
+                download(JSON.stringify(data), 'json.txt', 'text/plain');
+            }
+            , 30000);
+    };
+
+
+    static LatLangToArray(latLang) {
         return [latLang.lng, latLang.lat];
     }
 
-    ArrayOfLatLAngToArrayOfNumbers(LatLangArray) {
+    static ArrayOfLatLAngToArrayOfNumbers(LatLangArray) {
         let LatLangAsNumbers = [];
         for (let i = 0; i < LatLangArray.length; i++) {
-            LatLangAsNumbers[i] = this.LatLangToArray(LatLangArray[i]);
+            LatLangAsNumbers[i] = Map.LatLangToArray(LatLangArray[i]);
         }
         return LatLangAsNumbers;
     }
 
     componentDidMount() {
 
+        //var geoJsonLayer = L.geoJson(data, {
+        //    onEachFeature: function (feature, layer) {
+        //        if (layer instanceof L.Polyline) {
+        //            layer.setStyle({
+        //                'color': feature.properties.color
+        //            });
+        //        }
+        //    }
+        //});
+        //this.RouterControlRoadsLinesToJSON('asd');
         this.map = L.map("map", {
             center: [52.227932, 21.012843],
             zoom: 6,
             layers: [
                 L.tileLayer("https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png", {
                     attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                })
+                }),
+                //geoJsonLayer,
             ],
         });
-
+//
         L.layerGroup().addTo(this.map);
-
-        console.log('Array: ', this.LatLangToArray(L.latLng(53.444738, 14.524450)));
-        console.log(L.latLng(53.444738, 14.524450));
-
-
-        this.tempMap = L.map("tempMap", {
-            center: [51.55, 19.08],
-            zoom: 6,
-            layers: [
-                L.tileLayer("https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png", {
-                    attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                })
-            ],
-        });
-        L.layerGroup().addTo(this.tempMap);
-        let waypoints = [places[0], places[1]];
-        let control = L.routing.control({waypoints});
-        control.addTo(this.map);
-        control.on('routeselected', (e) => {
-
-            setTimeout(() => {
-                console.log('start adding: ');
-                let myLines = [{
-                    "type": "LineString",
-                    "coordinates": this.ArrayOfLatLAngToArrayOfNumbers(e.route.coordinates),
-                }];
-                console.log('myLines: ' + myLines);
-                L.geoJSON(myLines).addTo(this.tempMap);
-            }, 5000);
-        });
+//
+        this.RouterControlRoadsLinesToJSON('asd', places, this.map);
+        //console.log('Array: ', this.LatLangToArray(L.latLng(53.444738, 14.524450)));
+        //console.log(L.latLng(53.444738, 14.524450));
+//
+//
+        //this.tempMap = L.map("tempMap", {
+        //    center: [51.55, 19.08],
+        //    zoom: 6,
+        //    layers: [
+        //        L.tileLayer("https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png", {
+        //            attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        //        })
+        //    ],
+        //});
+        //L.layerGroup().addTo(this.tempMap);
+        //let waypoints = [places[0], places[1]];
+        //let control = L.routing.control({waypoints});
+        //control.addTo(this.map);
+        //control.on('routeselected', (e) => {
+//
+        //    setTimeout(() => {
+        //        console.log('start adding: ');
+        //        let myLines = [{
+        //            "type": "LineString",
+        //            "coordinates": this.ArrayOfLatLAngToArrayOfNumbers(e.route.coordinates),
+        //        }];
+        //        console.log('myLines: ' + myLines);
+        //        L.geoJSON(myLines).addTo(this.tempMap);
+        //    }, 5000);
+        //});
     }
 
 
